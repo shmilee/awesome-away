0f1d78ffaca542f4e6192ce44b6269fd7ae9ed87
shmilee <shmilee.zju@gmail.com>
2019-03-16 16:10:34 +0800

    init away and away.widget

---
+++ init.lua
@@ -0,0 +1,15 @@
+---------------------------------------------------------------------------
+--
+--  Away
+--  wallpapers, widgets and utilities for Awesome WM 4.x
+--
+--  Copyright (c) 2019 shmilee
+--  Licensed under GNU General Public License v2:
+--  https://opensource.org/licenses/GPL-2.0
+--
+---------------------------------------------------------------------------
+return {
+    wallpaper = require("away.wallpaper"),
+    widget = require("away.widget"),
+    util   = require("away.util"),
+}
---
+++ widget/init.lua
@@ -0,0 +1,10 @@
+---------------------------------------------------------------------------
+--
+--  Widget module for away: away.widget
+--
+--  Copyright (c) 2019 shmilee
+--  Licensed under GNU General Public License v2:
+--  https://opensource.org/licenses/GPL-2.0
+--
+---------------------------------------------------------------------------
+
