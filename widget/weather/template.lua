dc2c751ea51fd1c7462be3b2ab06719c403e8f2c
shmilee <shmilee.zju@gmail.com>
2019-03-20 12:47:43 +0800

    add weather widget template

---
+++ widget/weather/template.lua
@@ -0,0 +1,41 @@
+---------------------------------------------------------------------------
+--
+--  XXX weather widget for away: away.widget.weather.XXX
+--
+--  Copyright (c) 2019 shmilee
+--  Licensed under GNU General Public License v2:
+--  https://opensource.org/licenses/GPL-2.0
+--
+---------------------------------------------------------------------------
+
+local util = require("away.util")
+local base = require("away.widget.weather.base")
+local gfs  = require("gears.filesystem")
+
+local string = { gsub = string.gsub, format = string.format }
+
+-- XXX weather: fetch info from http://XXX
+local function get_XXXweather(args)
+    local args = args or {}
+    args.id    = args.id or 'XXX'
+    args.api   = args.api or 'http://'
+    args.query = args.query or { cityid=101210101 }
+    args.curl  = args.curl or 'curl -f -s -m 1.7'
+
+    -- set weather.now {wtype, forecast, etc} for setting
+    args.get_info  = args.get_info or function(weather, data)
+        weather.now.wtype = nil
+        weather.now.forecast = nil
+    end
+
+    -- edit weather.now {icon, text, notification_icon, notification_text}
+    args.setting = args.setting or function(weather)
+        if weather.now.wtype then
+            weather.now.notification_text = string.format('')
+        end
+    end
+
+    return base.worker(args)
+end
+
+return get_XXXweather
