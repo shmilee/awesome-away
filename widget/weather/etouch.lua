bfdafc3706cccc02590065474c99b68d073a861b
shmilee <shmilee.zju@gmail.com>
2019-03-20 12:48:54 +0800

    add weather old widget: etouch, xiaomiv2

---
+++ widget/weather/etouch.lua
@@ -0,0 +1,66 @@
+---------------------------------------------------------------------------
+--
+--  etouch weather widget for away: away.widget.weather.etouch
+--
+--  Copyright (c) 2019 shmilee
+--  Licensed under GNU General Public License v2:
+--  https://opensource.org/licenses/GPL-2.0
+--
+---------------------------------------------------------------------------
+
+local util = require("away.util")
+local base = require("away.widget.weather.base")
+local gfs  = require("gears.filesystem")
+
+local string = { gsub = string.gsub, format = string.format }
+
+-- etouchweather: fetch info from http://wthrcdn.etouch.cn
+local function get_etouchweather(args)
+    local args = args or {}
+    args.id    = args.id or 'etouch'
+    args.api   = args.api or 'http://wthrcdn.etouch.cn/weather_mini'
+    args.query = args.query or { citykey=101210101 }
+    args.curl  = args.curl or 'curl -f -s -m 1.7 --compressed'
+
+    -- set weather.now {desc, city, wtype, wendu, forecast}
+    args.get_info  = args.get_info or function(weather, data)
+        if data["desc"] == 'OK' then
+            weather.now.desc = true
+            weather.now.city = data['data']['city']
+            local wtype = data['data']['forecast'][1]['type']
+            weather.now.wtype = wtype
+            weather.now.wendu = data['data']['wendu'] .. '℃'
+            local forecast, now_forecast = data['data']['forecast'], ''
+            for i = 1, #forecast do
+                local day  = forecast[i]['date']
+                local tmin = string.gsub(forecast[i]['low'], '低温', '')
+                local tmax = string.gsub(forecast[i]['high'], '高温', '')
+                local desc = forecast[i]['type']
+                local wind = string.gsub(forecast[i]['fengli'], '.*DATA%[(.*)%]%].*', '%1')
+                wind = string.gsub(wind, '<', '&lt;')
+                wind = string.gsub(wind, '>', '&gt;')
+                util.print_debug(forecast[i]['fengli'] .. '-->' .. wind, args.id)
+                now_forecast = now_forecast .. string.format(
+                    "<b>%s</b>:  %s,  %s - %s,  %s",
+                    day, desc, tmin, tmax, wind)
+                if i < #forecast then
+                    now_forecast = now_forecast .. "\n"
+                end
+            end
+            weather.now.forecast = now_forecast
+        end
+    end
+
+    -- edit weather.now {icon, text, notification_icon, notification_text}
+    args.setting = args.setting or function(weather)
+        if weather.now.desc then
+            weather.now.notification_text = string.format(
+                "<b>%s %s</b>\n温度: %s\n%s",
+                 weather.now.city, weather.now.wtype, weather.now.wendu, weather.now.forecast)
+        end
+    end
+
+    return base.worker(args)
+end
+
+return get_etouchweather
---
+++ widget/weather/xiaomiv2.lua
@@ -0,0 +1,68 @@
+---------------------------------------------------------------------------
+--
+--  xiaomiv2 weather widget for away: away.widget.weather.xiaomiv2
+--
+--  Copyright (c) 2019 shmilee
+--  Licensed under GNU General Public License v2:
+--  https://opensource.org/licenses/GPL-2.0
+--
+---------------------------------------------------------------------------
+
+local util = require("away.util")
+local base = require("away.widget.weather.base")
+
+local os = { date = os.date, time = os.time }
+local string = { gsub = string.gsub, format = string.format }
+
+-- xiaomiv2weather: fetch info from https://weatherapi.market.xiaomi.com
+local function get_xiaomiv2weather(args)
+    local args = args or {}
+    args.id    = args.id or 'xiaomiv2'
+    args.api   = args.api or 'https://weatherapi.market.xiaomi.com/wtr-v2/weather'
+    args.query = args.query or { cityId=101210101 }
+    --args.curl  = args.curl or 'curl -f -s -m 1.7'
+
+    -- set weather.now {city, wtype, wendu, aql, forecast, etc} for setting
+    args.get_info  = args.get_info or function(weather, data)
+        if data['realtime'] then
+            weather.now.city = data['aqi']['city']
+            local wtype = data['realtime']['weather']
+            if wtype:match("转(.*)") then
+                wtype = wtype:match("转(.*)")
+            end
+            weather.now.wtype = wtype
+            weather.now.wendu = data['realtime']['temp'] .. '℃'
+            weather.now.aql = base.aqi2apl(data['aqi']['aqi'])
+        end
+        if data['forecast'] then
+            -- forecast.date_y == os.date('%Y年%m月%d日')
+            local now_forecast = ''
+            for i = 1, 6 do
+                local day  = os.date('%m月%d日', os.time()+60*60*24*(i-1))
+                local temp = data['forecast']['temp'.. i]
+                local desc = data['forecast']['weather'.. i]
+                local wind = data['forecast']['wind'.. i]
+                now_forecast = now_forecast .. string.format(
+                    "<b>%s</b>:  %s,  %s,  %s ",
+                    day, desc, temp, wind)
+                if i < 6 then
+                    now_forecast = now_forecast .. "\n"
+                end
+            end
+            weather.now.forecast = now_forecast                
+        end
+    end
+
+    -- edit weather.now {icon, text, notification_icon, notification_text}
+    args.setting = args.setting or function(weather)
+        if weather.now.wtype then
+            weather.now.notification_text = string.format(
+                "<b>%s %s</b>\n温度: %s\n%s\n%s",
+                weather.now.city, weather.now.wtype, weather.now.wendu, weather.now.aql, weather.now.forecast)
+        end
+    end
+
+    return base.worker(args)
+end
+
+return get_xiaomiv2weather
